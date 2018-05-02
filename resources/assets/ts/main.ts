3008d1e74107858fb244b32d38352d653ab90c65
Maike <maike_jbe@hotmail.com>
2018-05-01 22:25:39 -0300

    utilização das novas classes

--- resources/assets/ts/main.ts
+++ resources/assets/ts/main.ts
@@ -1,7 +1,18 @@
-import {TicTacToe} from "./Views/TicTacToe"
+import {TicTacToe} from "./Models/TicTacToe"
+import {Player} from "./Models/Player"
+import {Board} from "./Models/Board"
+import {Marker} from "./Enums/Marker"
 
 window.addEventListener("DOMContentLoaded", () => {
-    let canvas = <HTMLCanvasElement> document.querySelector("#game-canvas")
-    let ticTacToe = new TicTacToe(canvas)
-    ticTacToe.drawBoard()
+    let buttons = <NodeListOf<HTMLButtonElement>>document.querySelectorAll("button")
+
+    let player1 = new Player("Player 1", Marker.Xis)
+    let player2 = new Player("Player 2", Marker.Circle)
+    let players = {
+        player1,
+        player2
+    }
+    let board = new Board(buttons)
+    let game = new TicTacToe(players,board)
+    game.setcurrentPlayer(player1)
 })
