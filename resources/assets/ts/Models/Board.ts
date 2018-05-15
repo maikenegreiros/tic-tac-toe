63557feb2f3e6ff695a38d30ae41321c334ee48c
Maike <maike_jbe@hotmail.com>
2018-05-15 20:30:06 -0300

    new method and Observer pattern implementation

--- resources/assets/ts/Models/Board.ts
+++ resources/assets/ts/Models/Board.ts
@@ -1,24 +1,41 @@
 import {Player} from "./Player"
 import {Marker} from "../Enums/Marker"
 import {Subject} from "./../../../../node_modules/ts-observer-pattern/Subject"
+import {Observer} from "./../../../../node_modules/ts-observer-pattern/Observer"
 
-export class Board extends Subject
+export class Board extends Subject implements Observer
 {
     private xClass: string
     private circleClass: string
+    private container: HTMLElement
     private board: HTMLElement
+    private buttons: NodeListOf<HTMLButtonElement>
 
-    public constructor(board: HTMLElement)
+    public constructor(container: HTMLElement, board: HTMLElement, buttons: NodeListOf<HTMLButtonElement>)
     {
         super()
+        this.container = container
         this.board = board
+        this.buttons = buttons
         this.xClass = "xis"
         this.circleClass = "circle"
+    }
+
+    public update(data): this
+    {
+        this.container.classList.add(data.current)
+        this.container.classList.remove(data.next)
+        return this
     }
 
     public getBoard(): HTMLElement
     {
         return this.board
+    }
+
+    public getButtons(): NodeListOf<HTMLButtonElement>
+    {
+        return this.buttons
     }
 
     public mark(player: Player, button: HTMLButtonElement): void
